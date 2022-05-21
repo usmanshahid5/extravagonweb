4e7d691f58a948dba8b7e4314035c75559be979e
Abdullah <abdullah@intellicon.io>
2022-05-21 22:58:58 +0500

    make it better

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const dbConnection = require('./db');
+app.use(express.json());
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+  })
+}
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Node JS Server Started in Port ${ port }`));
