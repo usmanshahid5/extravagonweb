67cdf8d111cec36c82861c29de9eab37a86f2f30
Abdullah <abdullah@intellicon.io>
2022-05-22 01:16:04 +0500

    make it better

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use(express.json());
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
-
+  // production mode
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.get('/', (req, res) => res.send('Hello World!'));
