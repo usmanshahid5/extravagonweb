2fb43deb16f70c6298483fad9e535641ec8d4b16
Abdullah <abdullah@intellicon.io>
2022-05-22 10:07:31 +0500

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log(process.env.PORT, 111111111);
 
 if (process.env.NODE_ENV === 'production') {
   // production mode
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
